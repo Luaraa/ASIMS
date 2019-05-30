afff741e0b222fdc9536a762c2411e5ffb1b4c6b
Aoki <40490644+WQBin@users.noreply.github.com>
2019-05-31 00:28:46 +0800

    管理员信息管理模块及用户管理模块的添加

---
+++ ASIMS/ASIMS/Models/Method/AdminInforManagement.cs
@@ -0,0 +1,45 @@
+﻿//管理员及销售人员信息管理模块
+
+using ASIMS.Models;
+
+namespace AdminInfor
+{
+    class AdminInforManagement
+    {
+        /// <summary>
+        /// 处理管理员以及销售员工的登录请求
+        /// </summary>
+        /// <param name="id">管理员或销售员工用户账号</param>
+        /// <param name="password">管理员或销售员工用户密码</param>
+        /// <returns>可以登录返回true</returns>
+        public bool Login(string id,string password)
+        {
+            return false;
+        }
+        /// <summary>
+        /// 修改管理员或销售员工用户密码
+        /// </summary>
+        /// <param name="id">员工账号</param>
+        /// <param name="password">员工密码</param>
+        /// <returns>修改成功返回true</returns>
+        public bool NewPassword(string id,string password)
+        {
+            return false;
+        }
+        /// <summary>
+        /// 修改员工个人信息
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <param name="staff">员工（员工中的信息已经更新，需要加入数据库）</param>
+        /// <returns>修改成功返回true</returns>
+        public bool ModifyInformation(string id,Staffinfo staff)
+        {
+            return false;
+        }
+
+    }
+
+}
+
+
+
---
+++ ASIMS/ASIMS/Models/Method/UserManagement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/// <summary>
+/// 用户管理模块，实现的功能有，
+/// 1.列出所有的用户
+/// 2.查看某一用户的详细信息
+/// </summary>
+namespace ASIMS.Models.Method
+{
+    public class UserManagement
+    {
+        /// <summary>
+        /// 查看所有用户
+        /// </summary>
+        /// <returns>所有用户</returns>
+        public List<User> ListAllUser()
+        {
+            List<User> users = new List<User>();
+            #region
+
+            #endregion
+            return users;
+        }
+        /// <summary>
+        /// 查看账号为id用户的详细信息
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <returns>账号为id的用户</returns>
+        public User GetUserDetail(string id)
+        {
+            User user = new User();
+            #region
+
+
+            #endregion
+            return user;
+        }
+    }
+}
